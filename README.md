2eca772293f027104d612d5a5acb8bea1fb359da
brandonmizutani <bran2miz@gmail.com>
2024-04-10 18:04:12 -0700

    finished lecture, now working on the actual lab on my todo-app

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# React + Vite
+# Class 33 Demo
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+# Brandon Mizutani
 
-Currently, two official plugins are available:
+## Notes:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
--- src/Components/auth/Auth.jsx
+++ src/Components/auth/Auth.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+// import React from 'react';
 import {When} from 'react-if';
-
+import { useContext } from 'react';
 import { LoginContext } from './Context.jsx';
 
-class Auth extends React.Component {
+function Auth (props) {
 
-  static contextType = LoginContext;
+  const context = useContext(LoginContext);
+  // console.log(context);
 
-  render() {
-
-    const isLoggedIn = this.context.loggedIn;
-    const canDo = this.props.capability ? this.context.can(this.props.capability) : true;
+    const isLoggedIn = context.loggedIn;
+    const canDo = props.capability ? context.can(props.capability) : true;
     // canDo is based on capability
     // do they have permission to do the action
     const okToRender = isLoggedIn && canDo;
 
     return (
       <When condition={okToRender}>
-        {this.props.children}
+        {props.children}
       </When>
     );
-  }
+  
 }
 
 export default Auth;
